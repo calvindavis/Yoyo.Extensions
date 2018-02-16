978d97fa7c35379096e66bc5f2e01812d7f0799b
Calvin Davis <calvin.davis@yoyodesign.com>
2018-02-16 16:19:43 +0000

    Initial commit.

---
+++ src/Yoyo.Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Yoyo.Extensions
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Returns true if a string is null, empty, or contains only whitespace.
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public static bool IsEmpty(this string s)
+		{
+			return string.IsNullOrWhiteSpace(s);
+		}
+
+		/// <summary>
+		/// Returns true if a string is not null and contains at least one non-whitespace character.
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public static bool IsNotEmpty(this string s)
+		{
+			return !s.IsEmpty();
+		}
+
+		/// <summary>
+		/// Attemps to parse a string to an Enum.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="s"></param>
+		/// <param name="ignoreCase"></param>
+		/// <returns></returns>
+		public static T ToEnum<T>(this string s, bool ignoreCase = true)
+			where T : struct
+		{
+			var result = default(T);
+
+			if (s.IsNotEmpty())
+			{
+				Enum.TryParse(s, ignoreCase, out result);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ src/Yoyo.Tests/StringExtensionsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Yoyo.Extensions;
+
+namespace Yoyo.Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class StringExtensionsTest
+	{
+		[Test]
+		public void ToEnum()
+		{
+			var a = ((string) null).ToEnum<DayOfWeek>();
+			var b = "".ToEnum<DayOfWeek>();
+			var c = " ".ToEnum<DayOfWeek>();
+
+			Assert.AreEqual(default(DayOfWeek), a);
+			Assert.AreEqual(default(DayOfWeek), b);
+			Assert.AreEqual(default(DayOfWeek), c);
+		}
+
+		[Test]
+		public void ToEnum1()
+		{
+			var a = "Monday".ToEnum<DayOfWeek>();
+			var b = "monday".ToEnum<DayOfWeek>();
+
+			Assert.AreEqual(DayOfWeek.Monday, a);
+			Assert.AreEqual(DayOfWeek.Monday, b);
+		}
+
+		[Test]
+		public void ToEnum2()
+		{
+			var a = "Monday".ToEnum<DayOfWeek>(false);
+			var b = "monday".ToEnum<DayOfWeek>(false);
+
+			Assert.AreEqual(DayOfWeek.Monday, a);
+			Assert.AreEqual(default(DayOfWeek), b);
+		}
+	}
+}
