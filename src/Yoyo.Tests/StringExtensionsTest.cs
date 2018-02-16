97aa9ed07502a6767dc7e6b9fc95c241f9eae964
Calvin Davis <calvin.davis@yoyodesign.com>
2018-02-16 16:26:48 +0000

    Update StringExtensions tests to use NUnit's Constraint Model of assertions.

--- src/Yoyo.Tests/StringExtensionsTest.cs
+++ src/Yoyo.Tests/StringExtensionsTest.cs
@@ -8,36 +8,39 @@ namespace Yoyo.Tests
 	[TestFixture]
 	public class StringExtensionsTest
 	{
+		private static DayOfWeek DefaultDayOfWeek => default(DayOfWeek);
+		private static DayOfWeek Monday => DayOfWeek.Monday;
+
 		[Test]
-		public void ToEnum()
+		public void ToEnumStringIsEmpty()
 		{
 			var a = ((string) null).ToEnum<DayOfWeek>();
 			var b = "".ToEnum<DayOfWeek>();
 			var c = " ".ToEnum<DayOfWeek>();
 
-			Assert.AreEqual(default(DayOfWeek), a);
-			Assert.AreEqual(default(DayOfWeek), b);
-			Assert.AreEqual(default(DayOfWeek), c);
+			Assert.That(a, Is.EqualTo(DefaultDayOfWeek));
+			Assert.That(b, Is.EqualTo(DefaultDayOfWeek));
+			Assert.That(c, Is.EqualTo(DefaultDayOfWeek));
 		}
 
 		[Test]
-		public void ToEnum1()
+		public void ToEnum()
 		{
 			var a = "Monday".ToEnum<DayOfWeek>();
 			var b = "monday".ToEnum<DayOfWeek>();
 
-			Assert.AreEqual(DayOfWeek.Monday, a);
-			Assert.AreEqual(DayOfWeek.Monday, b);
+			Assert.That(a, Is.EqualTo(Monday));
+			Assert.That(b, Is.EqualTo(Monday));
 		}
 
 		[Test]
-		public void ToEnum2()
+		public void ToEnumIgnoreCaseIsFalse()
 		{
 			var a = "Monday".ToEnum<DayOfWeek>(false);
 			var b = "monday".ToEnum<DayOfWeek>(false);
 
-			Assert.AreEqual(DayOfWeek.Monday, a);
-			Assert.AreEqual(default(DayOfWeek), b);
+			Assert.That(a, Is.EqualTo(Monday));
+			Assert.That(b, Is.EqualTo(DefaultDayOfWeek));
 		}
 	}
 }
